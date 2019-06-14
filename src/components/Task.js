e9e9c48cb86813430a5fbb744a4901e642fb4e86
Radek <radexw1@gmail.com>
2019-06-14 16:48:59 +0200

    files ready to coding, redux ready, added new packages

---
+++ src/components/Task.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from '@material-ui/core/Card';
+
+const Task = () => {
+    return(
+        <>
+        <Card>Test card</Card>
+        <Card>Test card2</Card>
+        </>
+    )
+}
+
+export default Task;
---
+++ src/components/TaskList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Task from "./Task";
+
+const TaskList = () => {
+    return(
+        <div>
+            <h2>TaskList</h2>
+            <Task></Task>
+        </div>
+    )
+}
+
+export default TaskList;
